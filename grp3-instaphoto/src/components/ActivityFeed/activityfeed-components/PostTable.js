160e71af512978ce61da22e4e3bca38a5c83650d
jewc <jamescew@gmail.com>
2022-12-10 22:55:12 -0500

    Update PostTable to show public Posts only

--- grp3-instaphoto/src/components/ActivityFeed/activityfeed-components/PostTable.js
+++ grp3-instaphoto/src/components/ActivityFeed/activityfeed-components/PostTable.js
@@ -5,7 +5,6 @@ export default function PostTable (props){
    // counter to provide unique key to rows
    const counter = useRef(0);
    let postsList = [];
-   const usernameFilter = 'SHOW_ALL';
 
    // get the list of posts and the username from props
 
@@ -38,7 +37,7 @@ export default function PostTable (props){
       //     return;
       //   }
       // }
-      
+
       console.log("element.publicPrivate");
       console.log(element.publicPrivate);
       if(!element.publicPrivate===true) {
@@ -66,66 +65,3 @@ export default function PostTable (props){
 
 
 
-
-//   render() {
-//     return (
-//       <div>PostTable</div>
-//     )
-//   }
-//}
-
-/**
- 
-  function PostTable(props) {
-    // counter to provide unique key to rows
-    const counter = useRef(0);
-    let postsList = [];
-    let usernameFilter = 'SHOW_ALL';
-    // get the list of posts and the username from props
-
-    if (props.username) {
-      usernameFilter = props.username;
-    }
-    postsList = props.posts;
-
-    const makeRows = () => {
-      const rows = [];
-
-      postsList.forEach((element) => {
-        // const {post} = element;
-        if (usernameFilter === 'SHOW_ALL') {
-          rows.unshift(
-            <PostRow post={element}
-              key={counter.current}
-            />
-          );
-        } else {
-          if (!element.username.startsWith(usernameFilter)) {
-            return;
-          }
-          rows.unshift(
-            <PostRow post={element}
-              key={counter.current}
-            />,
-          );
-        }
-        // increment counter
-        counter.current += 1;
-
-      });
-      return rows;
-    };
-
-    const rows = makeRows();
-    return (
-      <table>
-        <tbody>{rows}</tbody>
-
-      </table>
-
-    );
-  }
-
-
- * 
- */
