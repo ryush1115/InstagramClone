971ac23d081bf783ef3e27b2b9738f1892cddb8a
jewc <jamescew@gmail.com>
2022-12-14 14:22:35 -0500

    Remove getToken from PostTable.js

--- grp3-instaphoto/src/components/ActivityFeed/activityfeed-components/PostTable.js
+++ grp3-instaphoto/src/components/ActivityFeed/activityfeed-components/PostTable.js
@@ -1,26 +1,25 @@
 import React, {useRef, useState, useEffect} from "react";
 import PostRow from './PostRow'
-import {getTokenUser} from "../../../api/mock_api";
 
 export default function PostTable (props){
    // counter to provide unique key to rows
    const counter = useRef(0);
    let postsList = [];
-   let followingList = [];
+   //let followingList = [];
 
 
-   const [user, setUser] = useState({});
+  //  const [user, setUser] = useState({});
 
-   useEffect(() => {
-       getTokenUser().then((user) => {
-           setUser(user.data);
-       });
-   }, []);
+  //  useEffect(() => {
+  //      getTokenUser().then((user) => {
+  //          setUser(user.data);
+  //      });
+  //  }, []);
 
    // get the list of posts and the username from props
 
    postsList = props.posts;
-   followingList = user.following;
+  //  followingList = user.following;
 
    const makeRows = () => {
    const rows = [];
